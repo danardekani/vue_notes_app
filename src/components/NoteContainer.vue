5118a8b4d9b09704e9e422edbc955704566bddb1
Daniel <ardekanicapital@gmail.com>
2019-08-15 21:30:39 -0400

    initial commit, create Toolbar, NoteContainer, NoteSelectors containers

---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,15 @@
+<template>
+ <div class="note-container">
+    <note-selectors />
+  </div>
+</template>
+
+<script>
+import NoteSelectors from "./NoteSelectors.vue";
+export default {
+  name: 'note-container',
+  components: {
+    NoteSelectors
+  } 
+};
+</script>
